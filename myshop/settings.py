9dd61012467f697acd079027f911b1dd8a73bd67
hoang duong <nhduong.dhti16a2hn@sv.uneti.edu.vn>
2026-03-30 17:07:40 +0700

    feat: django jazzmin

--- myshop/settings.py
+++ myshop/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok-free.dev']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
